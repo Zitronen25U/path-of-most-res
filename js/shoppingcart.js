50f5e7c7ab59d4fb14b2fae618edc4ca1789fec9
Piao <wenhaopi@08f8bc6186e1.ant.amazon.com>
2021-03-04 18:27:03 -0800

    Add a Product constructor function

--- js/shoppingcart.js
+++ js/shoppingcart.js
@@ -1,2 +1,21 @@
 'use strict';
 
+const allProducts = [];
+
+function Product(name, price, description, branch) {
+    this.name = name;
+    this.img = `../img/shoppingcart/${name}.jpg`;
+    this.price = price;
+    this.description = description;
+    this.branch = branch;
+    allProducts.push(this);
+}
+
+new Product('air force cup 1', 10, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', 'AF');
+new Product('air force pt shirt 1 (black)', 20, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', 'AF');
+new Product('air force pt shirt 1 (grey)', 20, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', 'AF');
+new Product('air force pt shorts 1 (light blue)', 15, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', 'AF');
+new Product('air force pt shorts 1 (solid blue)', 15, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', 'AF');
+new Product('air force training book 1', 7, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', 'AF');
+let imgs = document.getElementById('product-image');
+imgs.setAttribute('src', allProducts[0].img);
